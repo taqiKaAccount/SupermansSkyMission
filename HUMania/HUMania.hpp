bf3a72b18a6bbc0fa348a83050e584d7b96f4974
taqiKaAccount <122287731+taqiKaAccount@users.noreply.github.com>
2023-11-19 22:17:30 +0500

    Update HUMania.hpp
    added function Headers for flying obstacles and walking Objects

--- HUMania/HUMania.hpp
+++ HUMania/HUMania.hpp
@@ -10,6 +10,16 @@ void animation(SDL_Renderer* gRnderer, SDL_Texture* assets);
 
 void createObject(int x, int y);
 
+void drawObjectswalking(SDL_Renderer* gRnderer, SDL_Texture* assets);
+void animation_walking(SDL_Renderer* gRnderer, SDL_Texture* assets);
+void createObject_walking(int x, int y);
+void drawObjectswalking2(SDL_Renderer* gRnderer, SDL_Texture* assets);
+void createObject_walking2(int x, int y);
+
+
+void drawObjectsflying(SDL_Renderer* gRnderer, SDL_Texture* assets);
+void animation_flying(SDL_Renderer* gRnderer, SDL_Texture* assets);
+void createObject_flying(int x, int y);
 
 
 void moveRight();
