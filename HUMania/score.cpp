be0208e0af58f4612c8d1ef9eb94f181dd793a2f
taqiKaAccount <mt08073@st.habib.edu.pk>
2023-11-28 17:13:38 +0500

    working on score class

---
+++ HUMania/score.cpp
@@ -0,0 +1,4 @@
+#include "score.hpp"
+
+Score::Score(): ScoreCount(0) {};
+//score constructor
--- HUMania/score.hpp
+++ HUMania/score.hpp
@@ -1,11 +1,13 @@
 class Score{
 public:
+    Score() ;
 
+
+    //do op overlloading for increments/decrements of 10
 
 
 private:
     int ScoreCount = 0;
-
 
 
 }; 
