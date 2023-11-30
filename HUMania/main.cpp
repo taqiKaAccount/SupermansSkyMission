cf09dab53ea9a031c20ee28564a0bfeb6862711f
taqiKaAccount <mt08073@st.habib.edu.pk>
2023-11-30 22:38:03 +0500

    minor edits

--- HUMania/main.cpp
+++ HUMania/main.cpp
@@ -1,23 +1,5 @@
 #include "game.hpp"
 using namespace std;
-// int main(int argc, char *argv[]){
-//     Game game;
-//     srand(time(NULL));
-//     if( !game.init() ){
-// 		printf( "Failed to initialize!\n" );
-//         return 0;
-// 	}
-// 		//Load media
-//     if( !game.loadMedia() ){
-//         printf( "Failed to load media!\n" );
-//         return 0;
-//     }
-    
-//     game.run();
-//     game.close();
-
-//     return 0;
-// }
 
 
 int main(int argc, char* argv[]) {
@@ -25,12 +7,12 @@ int main(int argc, char* argv[]) {
     srand(time(NULL));
 
     if (!game.init()) {
-        cout << "Failed to initialize!\n";
+        std::cout << "Failed to initialize!\n";
         return 0;
     }
 
     if (!game.loadMedia()) {
-        cout << "Failed to load media!\n";
+        std::cout << "Failed to load media!\n";
         return 0;
     }
 
