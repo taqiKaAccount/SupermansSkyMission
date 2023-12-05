b6dc1c1cf1c9c0f1a0cccddbf1da06053edf14f5
taqiKaAccount <mt08073@st.habib.edu.pk>
2023-12-05 18:50:59 +0500

    start making music files

--- HUMania/Music.hpp
+++ HUMania/Music.hpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_mixer.h>
 
 
-class BackgroundMusic{};
+class BackgroundMusic{
+private:
+    Mix_Music* music;
+    // pointer to what music we want
+
+public:
+
+};
