779ebca2f78b248ac49e640d1d9e4be979b7eaec
taqiKaAccount <mt08073@st.habib.edu.pk>
2023-12-05 18:43:35 +0500

    changes

--- HUMania/drawing.hpp
+++ HUMania/drawing.hpp
@@ -1,12 +1,12 @@
-#include<SDL.h>
-#pragma once
-class Drawing{
-    public:
-    //The window renderer
-    static SDL_Renderer* gRenderer;
-    //global reference to png image sheets
-    static SDL_Texture* assets;
-};
+// #include<SDL.h>
+// #pragma once
+// class Drawing{
+//     public:
+//     //The window renderer
+//     static SDL_Renderer* gRenderer;
+//     //global reference to png image sheets
+//     static SDL_Texture* assets;
+// };
 
 
 
