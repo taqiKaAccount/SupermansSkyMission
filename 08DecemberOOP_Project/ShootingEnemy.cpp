4bc0168a420bf1e2424d67155119c7c2b6cb321d
taqiKaAccount <mt08073@st.habib.edu.pk>
2023-12-08 17:38:48 +0500

    shooting enemy class with features taken from existing stuff

--- 08DecemberOOP_Project/ShootingEnemy.cpp
+++ 08DecemberOOP_Project/ShootingEnemy.cpp
@@ -1,2 +1,46 @@
 #pragma once
 #include "ShootingEnemy.hpp"
+
+ShootingEnemy::ShootingEnemy(int x, int y) : Unit(){
+    Unit::srcRect = {7,88,160,103};
+    Unit::moverRect = {x, y, 50, 50};
+}
+
+bool ShootingEnemy::del_child(){
+    if(moverRect.x >= 999){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+
+// fly() is overrided from the superclass
+void ShootingEnemy::fly(){
+    // 
+    moverRect.x += speed;
+    speed += 0.5; //slowly increase speed as time passes to increase difficulty
+    if(moverRect.x > 999){
+        moverRect.x = 0;
+    } 
+    if (srcRect.x==7) {
+        srcRect.x=0;
+        srcRect.y=237;
+        srcRect.w=153;
+        srcRect.h=84;
+    } else if (srcRect.x==0) {
+        srcRect.x=2;
+        srcRect.y=361;
+        srcRect.w=159;
+        srcRect.h=124;
+    } else if (srcRect.x==2) {
+        srcRect.x=7;
+        srcRect.y=88;
+        srcRect.w=155;
+        srcRect.h=103;
+    }
+
+}
--- 08DecemberOOP_Project/ShootingEnemy.hpp
+++ 08DecemberOOP_Project/ShootingEnemy.hpp
@@ -2,3 +2,12 @@
 #include<SDL.h>
 #include "drawing.hpp"
 #include "unit.hpp"
+
+class ShootingEnemy : public Unit{
+    public:
+    char name = 'S';
+    bool del_child();
+    ShootingEnemy(int x = 0, int y = 0);
+    void fly();
+    int speed = 0.5;
+};
